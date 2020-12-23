7191a99667c1f4362be215a152dc38894aee8a25
Ben Lirio <ben@bens-mbp.lan>
2020-12-23 11:19:37 -0500

    Add roomba movements

--- sandbox/logic/roomba/room/room.go
+++ sandbox/logic/roomba/room/room.go
@@ -1,6 +1,7 @@
 package room
 
 import (
+	"log"
 	"math/rand"
 )
 
@@ -11,10 +12,62 @@ const (
 	DOWN
 	LEFT
 )
+func (d Direction) GetVector() Vector {
+	switch d {
+	case UP:
+		return newVector(-1, 0)
+	case RIGHT:
+		return newVector(0, 1)
+	case DOWN:
+		return newVector(1, 0)
+	case LEFT:
+		return newVector(0, -1)
+	default:
+		log.Fatalf("Direction should be one of UP, RIGHT, DOWN, LEFT. Got %d\n", d)
+		return newVector(0, 0)
+	}
+}
+type Row []bool
+type Matrix []Row
+func newMatrix(x int, y int) Matrix {
+	if x == 0 || y == 0 {
+		log.Fatal("Can not make matrix of size 0,0")
+	}
+	matrix := make(Matrix, x)
+	for i := 0; i < x; i++ {
+		matrix[i] = make(Row, y)
+	}
+	return matrix
+}
+func (m Matrix) Get(v Vector) bool {
+	return m[v.x][v.y]
+}
+func (m Matrix) Set(v Vector, val bool) {
+	m[v.x][v.y] = val
+}
+func (m Matrix) Size() Vector {
+	return newVector(len(m), len(m[0]))
+}
+func (m Matrix) InBounds(v Vector) bool {
+	size := m.Size()
+	if v.x < 0 || v.y < 0 { return false }
+	if v.x >= size.x || v.y >= size.y { return false }
+	return true
+}
 
 type Vector struct {
 	x int
 	y int
+}
+func newVector(x int, y int) Vector {
+	return Vector{x,y}
+}
+func (v1 Vector) Add(v2 Vector) Vector {
+	return newVector(v1.x + v2.x, v1.y + v2.y)
+}
+
+func (v Vector) Copy() Vector {
+	return newVector(v.x, v.y)
 }
 
 type Roomba struct {
@@ -25,49 +78,42 @@ type Roomba struct {
 type Room struct {
 	size Vector
 	roomba Roomba
-	walls [][]bool
+	walls Matrix
+	clean Matrix
 }
 
 
 func newRoom(x int, y int, seed int64) *Room {
 	rand.Seed(seed)
-	room := Room{}
-	room.size = Vector{x, y}
-	room.walls = make([][]bool, x)
-	for i := 0; i < x; i++ {
-		room.walls[i] = make([]bool, y)
-	}
+	var room Room
+	room.size = newVector(x, y)
+	room.walls = newMatrix(x, y)
 	room.roomba = Roomba{direction: UP}
-	start := Vector{rand.Intn(x), rand.Intn(y)}
+	start := newVector(rand.Intn(x), rand.Intn(y))
 	room.roomba.pos = start
-	var isUnreachable func() bool
 
+	var isUnreachable func() bool
 	isUnreachable = func() bool {
-		visited := make([][]bool, x)
-		for i := 0; i < x; i++ {
-			visited[i] = make([]bool, y)
-		}
-		var traverse func(int, int)
-		traverse = func(x int, y int) {
-			if room.walls[x][y] == true {
-				return
-			}
+		visited := newMatrix(x, y)
+		var traverse func(Vector)
+		traverse = func(pos Vector) {
+			if room.walls.Get(pos) == true { return }
 			for i := -1; i <= 1; i++ {
 				for j := -1; j <= 1; j++ {
-					xoff := x + i
-					yoff := y + j
-					if xoff < 0 || xoff >= len(visited) { continue }
-					if yoff < 0 || yoff >= len(visited[0]) { continue }
-					if visited[xoff][yoff] == true { continue }
-					visited[xoff][yoff] = true
-					traverse(xoff, yoff)
+					idx := newVector(i, j)
+					offset := pos.Add(idx)
+					if visited.InBounds(offset) == false { continue }
+					if visited.Get(offset) == true { continue }
+					visited.Set(offset, true)
+					traverse(offset)
 				}
 			}
 		}
-		traverse(room.roomba.pos.x, room.roomba.pos.y)
+		traverse(room.roomba.pos)
 		for i := 0; i < len(visited); i++ {
 			for j := 0; j < len(visited[i]); j++ {
-				if visited[i][j] == false {
+				idx := newVector(i, j)
+				if visited.Get(idx) == false {
 					return true
 				}
 			}
@@ -76,13 +122,53 @@ func newRoom(x int, y int, seed int64) *Room {
 	}
 	for i := 0; i < x; i++ {
 		for j := 0; j < y; j++ {
+			idx := newVector(i, j)
 			v := rand.Intn(2)
 			if v == 0 { continue }
-			if room.roomba.pos.x == i && room.roomba.pos.y == j { continue }
-			room.walls[i][j] = true
+			//if room.roomba.pos.x == idx.x && room.roomba.pos.y == idx.y { continue }
+			if room.roomba.pos == idx { continue }
+			room.walls.Set(idx, true)
 			if isUnreachable() == false { continue }
-			room.walls[i][j] = false
+			room.walls.Set(idx, false)
+		}
+	}
+	room.clean = newMatrix(x, y)
+	for i := 0; i < x; i++ {
+		for j := 0; j < y; j++ {
+			idx := newVector(i, j)
+			if room.walls.Get(idx) == true {
+				room.clean.Set(idx, true)
+			}
 		}
 	}
 	return &room
+}
+func (room *Room) IsClean() bool {
+	size := room.clean.Size()
+	for i := 0; i < size.x; i++ {
+		for j := 0; j < size.y; j++ {
+			idx := newVector(i, j)
+			if room.clean.Get(idx) == false {
+				return false
+			}
+		}
+	}
+	return true
+}
+func (room *Room) Clean() {
+	room.clean.Set(room.roomba.pos, true)
+}
+func (room *Room) Right() {
+	room.roomba.direction = (room.roomba.direction + 1) % 4
+}
+func (room *Room) Left() {
+	room.roomba.direction = (room.roomba.direction - 1) % 4
+}
+func (room *Room) Forward() bool {
+	directionVector := room.roomba.direction.GetVector()
+	prevPos := room.roomba.pos
+	room.roomba.pos = room.roomba.pos.Add(directionVector)
+	if room.walls.Get(room.roomba.pos) == false { return true }
+	room.roomba.pos = prevPos
+	return false
 }
--- sandbox/logic/roomba/room/room_test.go
+++ sandbox/logic/roomba/room/room_test.go
@@ -85,3 +85,130 @@ func TestRoomTraversable(t *testing.T) {
 		t.Errorf("Warning no walls generated")
 	}
 }
+
+func TestIsClean(t *testing.T) {
+	room := newRoom(10, 11, 42)
+	for i := 0; i < len(room.walls); i++ {
+		for j := 0; j < len(room.walls[0]); j++ {
+			if room.walls[i][j] == false && room.clean[i][j] == true {
+				t.Errorf("Spot (%d, %d) has no wall so should not start clean\n", i, j)
+			}
+			if room.walls[i][j] == true && room.clean[i][j] == false {
+				t.Errorf("Spot (%d, %d) has a wall so it should start clean\n", i, j)
+			}
+		}
+	}
+	if room.IsClean() == true {
+		t.Errorf("Room should not start clean\n")
+	}
+	for i := 0; i < len(room.walls); i++ {
+		for j := 0; j < len(room.walls[0]); j++ {
+			room.clean[i][j] = true
+		}
+	}
+	if room.IsClean() == false {
+		t.Errorf("Room should be clean when all squares are\n")
+	}
+}
+
+func TestRoombaClean(t *testing.T) {
+	room := newRoom(10, 10, 42)
+	numClean := 0
+	for i := 0; i < len(room.walls); i++ {
+		for j := 0; j < len(room.walls[0]); j++ {
+			if room.clean[i][j] == true {
+				numClean += 1
+			}
+		}
+	}
+	room.Clean()
+	numClean2 := 0
+	for i := 0; i < len(room.walls); i++ {
+		for j := 0; j < len(room.walls[0]); j++ {
+			if room.clean[i][j] == true {
+				numClean2 += 1
+			}
+		}
+	}
+	room.Clean()
+	numClean3 := 0
+	for i := 0; i < len(room.walls); i++ {
+		for j := 0; j < len(room.walls[0]); j++ {
+			if room.clean[i][j] == true {
+				numClean3 += 1
+			}
+		}
+	}
+	if numClean != (numClean2 - 1) {
+		t.Errorf("Roomba should clean the square it is on and only that square\n")
+	}
+	if numClean2 != numClean3 {
+		t.Errorf("Cleaning the same square twice should not do anything\n")
+	}
+}
+
+func TestRotation(t *testing.T) {
+	room := newRoom(8, 9, 42)
+	if room.roomba.direction != UP {
+		t.Errorf("Roomba should start going UP\n")
+	}
+	room.Right()
+	if room.roomba.direction != RIGHT {
+		t.Errorf("UP => Right() sould be RIGHT\n")
+	}
+	room.Right()
+	if room.roomba.direction != DOWN {
+		t.Errorf("RIGHT => Right() sould be DOWN\n")
+	}
+	room.Right()
+	if room.roomba.direction != LEFT {
+		t.Errorf("DOWN => Right() sould be LEFT\n")
+	}
+	room.Right()
+	if room.roomba.direction != UP {
+		t.Errorf("LEFT => Right() sould be UP\n")
+	}
+	room.Left()
+	if room.roomba.direction != LEFT {
+		t.Errorf("UP => Left() sould be LEFT\n")
+	}
+	room.Left()
+	if room.roomba.direction != DOWN {
+		t.Errorf("LEFT => Left() sould be LEFT\n")
+	}
+	room.Left()
+	if room.roomba.direction != RIGHT {
+		t.Errorf("DOWN => Left() sould be RIGHT\n")
+	}
+	room.Left()
+	if room.roomba.direction != UP {
+		t.Errorf("RIGHT => Left() sould be UP\n")
+	}
+}
+
+func TestMove(t *testing.T) {
+	room := newRoom(6, 8, 42)
+	room.roomba.pos.x = 0
+	room.roomba.pos.y = 0
+	room.roomba.direction = DOWN
+	room.walls[0][0] = false
+	room.walls[1][0] = false
+	room.walls[2][0] = true
+	if room.roomba.pos.x != 0 || room.roomba.pos.y != 0 {
+		t.Errorf("Should have put roomba in (0, 0)\n")
+	}
+	ok := room.Forward()
+	if ok == false {
+		t.Errorf("Forward() should return true when there is no wall\n")
+	}
+	if room.roomba.pos.x != 1 || room.roomba.pos.y != 0 {
+		t.Errorf("Roomba should be a pos (1, 0), got %v\n", room.roomba.pos)
+	}
+	ok = room.Forward()
+	if ok == true {
+		t.Errorf("Forward() should return false when there is a wall\n")
+	}
+	if room.roomba.pos.x != 1 || room.roomba.pos.y != 0 {
+		t.Errorf("Roomba should be a pos (1, 0), got %v\n", room.roomba.pos)
+	}
+}
