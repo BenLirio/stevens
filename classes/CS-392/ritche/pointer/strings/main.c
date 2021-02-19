50b2239ef0006f9759869c5169dcef5537bd181a
Ben Lirio <benjlirio@gmail.com>
2021-02-19 09:42:45 -0500

    pointers-strings

---
+++ classes/CS-392/ritche/pointer/strings/main.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+void my_strcat(char *a, char *b)
+{
+
+}
+
+int main()
+{
+	char *a = "1234";
+	char *b = "567";
+	my_strcat(a, b);
+	printf("a: %s\n", a);
+	return 0;
+}
