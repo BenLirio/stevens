42d44ce4c014fe0d3d4a72eee59c648b3ee49b17
Ben Lirio <benjlirio@gmail.com>
2021-02-22 13:12:15 -0500

    Quicksort init

---
+++ classes/CS-392/quicksort/quicksort.h
@@ -0,0 +1,11 @@
+/*******************************************************************************
+ * Name        : quicksort.h
+ * Author      : Ben Lirio
+ * Date        : 2/22/21
+ * Description : Quicksort header.
+ * Pledge      : I pledge my honor that I have abided by the Stevens Honor System
+ ******************************************************************************/
+/**
+ * TODO - put all non-static function prototypes from quicksort.c inside
+ * wrapper #ifndef.
+ */
